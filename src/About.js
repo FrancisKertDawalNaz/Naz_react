dd80c94a5c05f4c710310a75fedccf75a02d8169
kert <nfranciskert@gmail.com>
2025-05-06 10:55:00 +0800

    add new 2 files

---
+++ src/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function About(){
+    return(
+       <div>Welcome to About page</div>
+    );
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Naz from './naz';
+import Home from './Home'; // You'll need to create this
+import About from './About'; // And this too
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <Naz />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul style={{ listStyle: 'none', display: 'flex', gap: '1rem' }}>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/naz">Naz</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+        </nav>
+        <header className="App-header">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/naz" element={<Naz />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
